ab41f8a95cef42799070e7f021db175480ec3429
Roman Cherednychenko <Roman_Cherednichenko@epam.com>
2017-04-14 12:25:41 +0300

    fix markup

--- src/main/resources/flickr2blog/launcher.js
+++ src/main/resources/flickr2blog/launcher.js
@@ -1,4 +1,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
+//    window.open('https://www.google.com'); 
+//    window.open('flickr2blog.html');
+    
   chrome.app.window.create('flickr2blog.html', {
      id: 'MainWindowId',
      'outerBounds': {
